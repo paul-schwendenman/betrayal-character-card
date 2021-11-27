21ce05a048f5571e4d28b2de9052d1448bbbff1b
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-27 00:55:41 -0500

    Add character list to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import characters from '$lib/characters';
+</script>
+
+<div class="content">
+	<h1>Characters</h1>
+	<ul>
+		{#each characters as character, index (character.name)}
+			<li>
+				<a href="/c/{index}">{character.name}</a>
+			</li>
+		{/each}
+	</ul>
+</div>
+
+<style>
+	.content {
+		margin: auto;
+		max-width: 720px;
+	}
+</style>
