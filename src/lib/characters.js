d5937fab5019074032ccdbbb800c7859b0f4e36c
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-27 01:55:08 -0500

    Add remaining character stats

--- src/lib/characters.js
+++ src/lib/characters.js
@@ -60,4 +60,307 @@ export default [
 			}
 		}
 	},
+	{
+		name: "Missy Dubourde",
+		color: "yellow",
+		age: "9",
+		height: "4'2\"",
+		weight: "62",
+		hobbies: [
+			"Swimming",
+			"Medicine"
+		],
+		dob: "02-14",
+		traits: {
+			speed: {
+				values: ['💀', 3, 4, 5, 6, 6, 6, 7, 7],
+				startingIndex: 3
+			},
+			might: {
+				values: ['💀', 2, 3, 3, 3, 4, 5, 6, 7],
+				startingIndex: 4
+			},
+			sanity: {
+				values: ['💀', 1, 2, 3, 4, 5, 5, 6, 7],
+				startingIndex: 3,
+			},
+			knowledge: {
+				values: ['💀', 2, 3, 4, 4, 5, 6, 6, 6],
+				startingIndex: 4
+			}
+		},
+	},
+	{
+		name: "Zoe Ingstrom",
+		color: "yellow",
+		age: "8",
+		height: "3'9\"",
+		weight: "49",
+		hobbies: [
+			"Dolls",
+			"Music"
+		],
+		dob: "11-05",
+		traits: {
+			speed: {
+				values: ['💀', 4, 4, 4, 4, 5, 6, 8, 8],
+				startingIndex: 4
+			},
+			might: {
+				values: ['💀', 2, 2, 3, 3, 4, 4, 6, 7],
+				startingIndex: 4
+			},
+			sanity: {
+				values: ['💀', 3, 4, 5, 5, 6, 6, 7, 8],
+				startingIndex: 3,
+			},
+			knowledge: {
+				values: ['💀', 1, 2, 3, 4, 4, 5, 5, 5],
+				startingIndex: 3
+			}
+		},
+	},
+	{
+		name: "Heather Granville",
+		color: "purple",
+		age: "18",
+		height: "5'2\"",
+		weight: "120",
+		hobbies: [
+			"Television",
+			"Shopping"
+		],
+		dob: "08-02",
+		traits: {
+			speed: {
+				values: ['💀', 3, 3, 4, 5, 6, 6, 7, 8],
+				startingIndex: 3
+			},
+			might: {
+				values: ['💀', 3, 3, 3, 4, 5, 6, 7, 8],
+				startingIndex: 3
+			},
+			sanity: {
+				values: ['💀', 3, 3, 3, 4, 5, 6, 6, 6],
+				startingIndex: 3,
+			},
+			knowledge: {
+				values: ['💀', 2, 3, 3, 4, 5, 6, 7, 8],
+				startingIndex: 5
+			}
+		},
+	},
+	{
+		name: "Jenny LeClerc",
+		color: "purple",
+		age: "21",
+		height: "5'7\"",
+		weight: "142",
+		hobbies: [
+			"Reading",
+			"Soccer"
+		],
+		dob: "03-04",
+		traits: {
+			speed: {
+				values: ['💀', 2, 3, 4, 4, 4, 5, 6, 8],
+				startingIndex: 4
+			},
+			might: {
+				values: ['💀', 3, 4, 4, 4, 4, 5, 6, 8],
+				startingIndex: 3
+			},
+			sanity: {
+				values: ['💀', 1, 1, 2, 4, 4, 4, 5, 6],
+				startingIndex: 5,
+			},
+			knowledge: {
+				values: ['💀', 2, 3, 3, 4, 4, 5, 6, 8],
+				startingIndex: 3
+			}
+		},
+	},
+	{
+		name: "Peter Akimoto",
+		color: "green",
+		age: "13",
+		height: "4'11\"",
+		weight: "98",
+		hobbies: [
+			"Bugs",
+			"Basketball"
+		],
+		dob: "09-03",
+		traits: {
+			speed: {
+				values: ['💀', 3, 3, 3, 4, 6, 6, 7, 7],
+				startingIndex: 4
+			},
+			might: {
+				values: ['💀', 2, 3, 3, 4, 5, 5, 6, 8],
+				startingIndex: 3
+			},
+			sanity: {
+				values: ['💀', 3, 4, 4, 4, 5, 6, 6, 7],
+				startingIndex: 4,
+			},
+			knowledge: {
+				values: ['💀', 3, 4, 4, 5, 6, 7, 7, 8],
+				startingIndex: 3
+			}
+		},
+	},
+	{
+		name: "Brandon Jaspers",
+		color: "green",
+		age: "12",
+		height: "5'1\"",
+		weight: "109",
+		hobbies: [
+			"Computers",
+			"Camping",
+			"Hockey"
+		],
+		dob: "05-21",
+		traits: {
+			speed: {
+				values: ['💀', 3, 4, 4, 4, 5, 6, 7, 8],
+				startingIndex: 3
+			},
+			might: {
+				values: ['💀', 2, 3, 3, 4, 5, 6, 6, 7],
+				startingIndex: 4
+			},
+			sanity: {
+				values: ['💀', 3, 3, 3, 4, 5, 6, 7, 8],
+				startingIndex: 4,
+			},
+			knowledge: {
+				values: ['💀', 1, 3, 3, 5, 5, 6, 6, 7],
+				startingIndex: 3
+			}
+		},
+	},
+	{
+		name: "Darrin \"Flash\" Willams",
+		color: "red",
+		age: "20",
+		height: "5'11\"",
+		weight: "188",
+		hobbies: [
+			"Track",
+			"Music",
+			"Shakespearean Literature"
+		],
+		dob: "06-06",
+		traits: {
+			speed: {
+				values: ['💀', 4, 4, 4, 5, 6, 7, 7, 8],
+				startingIndex: 5
+			},
+			might: {
+				values: ['💀', 2, 3, 3, 4, 5, 6, 6, 7],
+				startingIndex: 3
+			},
+			sanity: {
+				values: ['💀', 1, 2, 3, 4, 5, 5, 5, 7],
+				startingIndex: 3,
+			},
+			knowledge: {
+				values: ['💀', 2, 3, 3, 4, 5, 5, 5, 7],
+				startingIndex: 3
+			}
+		},
+	},
+	{
+		name: "Ox Bellows",
+		color: "red",
+		age: "23",
+		height: "6'4\"",
+		weight: "288",
+		hobbies: [
+			"Footbal",
+			"Shiny Objects"
+		],
+		dob: "08-18",
+		traits: {
+			speed: {
+				values: ['💀', 2, 2, 2, 3, 4, 5, 5, 6],
+				startingIndex: 5
+			},
+			might: {
+				values: ['💀', 4, 5, 5, 6, 6, 7, 8, 8],
+				startingIndex: 3
+			},
+			sanity: {
+				values: ['💀', 2, 2, 3, 4, 5, 5, 6, 7],
+				startingIndex: 3
+			},
+			knowledge: {
+				values: ['💀', 2, 2, 3, 3, 5, 5, 6, 6],
+				startingIndex: 3
+			}
+		},
+	},
+	{
+		name: "Madame Zostra",
+		color: "blue",
+		age: "37",
+		height: "5'0\"",
+		weight: "150",
+		hobbies: [
+			"Astrology",
+			"Cooking",
+			"Baseball"
+		],
+		dob: "12-10",
+		traits: {
+			speed: {
+				values: ['💀', 2, 3, 3, 5, 5, 6, 6, 7],
+				startingIndex: 3
+			},
+			might: {
+				values: ['💀', 2, 3, 3, 4, 5, 5, 5, 6],
+				startingIndex: 4
+			},
+			sanity: {
+				values: ['💀', 4, 4, 4, 5, 6, 7, 8, 8],
+				startingIndex: 3,
+			},
+			knowledge: {
+				values: ['💀', 1, 3, 4, 4, 4, 5, 6, 6],
+				startingIndex: 4
+			}
+		},
+	},
+	{
+		name: "Vivian Lopez",
+		color: "blue",
+		age: "42",
+		height: "5'5\"",
+		weight: "142",
+		hobbies: [
+			"Old Movies",
+			"Horses"
+		],
+		dob: "01-11",
+		traits: {
+			speed: {
+				values: ['💀', 3, 4, 4, 4, 4, 6, 7, 8],
+				startingIndex: 4
+			},
+			might: {
+				values: ['💀', 2, 2, 2, 4, 4, 5, 6, 6],
+				startingIndex: 3
+			},
+			sanity: {
+				values: ['💀', 4, 4, 4, 5, 6, 7, 8, 8],
+				startingIndex: 5,
+			},
+			knowledge: {
+				values: ['💀', 4, 5, 5, 5, 5, 6, 6, 7],
+				startingIndex: 4
+			}
+		},
+	},
 ];
