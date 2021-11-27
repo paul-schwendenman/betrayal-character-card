9eb438543e073e957a059bfa34cadc1073fd9c9b
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-27 02:02:42 -0500

    Add summary idea to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,11 @@
 	<ul>
 		{#each characters as character, index (character.name)}
 			<li>
-				<a href="/c/{index}">{character.name}</a>
+				<a href="/c/{index}">{character.name} </a>
+				<!-- speed: {character.traits.speed.values[character.traits.speed.startingIndex]} -->
+				<!-- might: {character.traits.might.values[character.traits.might.startingIndex]} -->
+				<!-- sanity: {character.traits.sanity.values[character.traits.sanity.startingIndex]} -->
+				<!-- knowledge: {character.traits.knowledge.values[character.traits.knowledge.startingIndex]} -->
 			</li>
 		{/each}
 	</ul>
