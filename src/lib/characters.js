7c35c12aaf2729389baeacc72a54d154ff7a2a41
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-26 23:57:01 -0500

    Add white color characters

---
+++ src/lib/characters.js
@@ -0,0 +1,59 @@
+export default [
+	{
+		name: "Father Rhinehardt",
+		color: "white",
+		age: "62",
+		height: "5'9\"",
+		weight: "185",
+		hobbies: [
+			"Fencing",
+			"Gardening"
+		],
+		dob: "04-29",
+		speed: {
+			values: ['💀', 2, 3, 3, 4, 5, 6, 7, 7],
+			startingIndex: 3
+		},
+		might: {
+			values: ['💀', 1, 2, 2, 4, 4, 5, 5, 7],
+			startingIndex: 3
+		},
+		sanity: {
+			values: ['💀', 3, 4, 5, 5, 6, 7, 7, 8],
+			startingIndex: 5,
+		},
+		knowledge: {
+			values: ['💀', 1, 3, 3, 4, 5, 6, 6, 8],
+			startingIndex: 4
+		}
+	},
+	{
+		name: "Professor Longfellow",
+		color: "white",
+		age: "57",
+		height: "5'11\"",
+		weight: "153",
+		hobbies: [
+			"Gaelic Music",
+			"Drama",
+			"Fine Wines"
+		],
+		dob: "07-27",
+		speed: {
+			values: ['💀', 2, 2, 4, 4, 5, 5, 6, 6],
+			startingIndex: 4
+		},
+		might: {
+			values: ['💀', 1, 2, 3, 4, 5, 5, 6, 6],
+			startingIndex: 3
+		},
+		sanity: {
+			values: ['💀', 1, 3, 3, 4, 5, 5, 6, 7],
+			startingIndex: 3,
+		},
+		knowledge: {
+			values: ['💀', 4, 5, 5, 5, 5, 6, 7, 8],
+			startingIndex: 5
+		}
+	},
+];
