e509c72a857b6f657e8048d42722d79b41d3037c
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-27 00:40:12 -0500

    Add character route

---
+++ src/routes/c/[index].svelte
@@ -0,0 +1,26 @@
+<script context="module">
+import characters from "$lib/characters";
+
+
+	export async function load({ page }) {
+		const { index } = page.params;
+
+		const character = characters[index];
+
+		return {
+			props: { character }
+		};
+	}
+</script>
+
+<script>
+	import Character from "$lib/Character.svelte";
+
+	export let character;
+</script>
+
+<svelte:head>
+	<title>{character.name}</title>
+</svelte:head>
+
+<Character {...character}></Character>
